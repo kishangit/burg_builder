de4709ea22c86b4b5de11cd21a8bf0b7eebe9f04
Kishan Dalwadi <kdalwadi@plumslice.com>
2020-08-29 15:29:01 +0530

    Completed till 350

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -3,7 +3,6 @@ import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSumm
 import { connect } from 'react-redux';
 import { Route, Redirect } from 'react-router-dom';
 import ContactData from './ContactData/ContactData';
-import * as actions from '../../store/actions/index';
 
 class Checkout extends Component {
 
--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -27,7 +27,6 @@ export const purchaseBurger = (orderData, token) => {
         dispatch(purchaseBurgerStart());
         axios.post('/orders.json?auth=' + token, orderData )
             .then(response => {
-                console.log(response.data);
                 dispatch(purchaseBurgerSuccess(response.data.name, orderData));
             })
             .catch(error => {
@@ -75,7 +74,6 @@ export const fetchOrders = (token, userId) => {
                     id: key 
                 });
             }
-            console.log(res.data);
             dispatch(fetchOrdersSuccess(fetchedOrders));
             this.setState({loading: false, orders: fetchedOrders});
         })
