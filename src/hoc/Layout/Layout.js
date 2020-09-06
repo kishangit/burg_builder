dee983f1f36171a5b2a4f8c76270eb6603ef53aa
Kishan Dalwadi <kdalwadi@plumslice.com>
2020-09-06 09:58:45 +0530

    454, 455

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,41 +1,35 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import Aux from '../Auxilliary/Auxilliary';
 import classes from './Layout.module.css';
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
 import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
 
-class Layout extends Component {
-    state = {
-        showSideDrawer: false
+const Layout = props => {
+    const [ sideDrawerIsVisible, setSideDrawerIsVisible ] = useState(false);
+
+    const sideDrawerClosedHandler = () => {
+        setSideDrawerIsVisible(false);
     }
 
-    sideDrawerClosedHandler = () => {
-        this.setState({showSideDrawer: false});
+    const sideDrawerToggleHandler = () => {
+        setSideDrawerIsVisible(!sideDrawerIsVisible);
     }
 
-    sideDrawerToggleHandler = () => {
-        this.setState((prevState) => {
-            return {showSideDrawer: !prevState.showSideDrawer};
-        })
-    }
-
-    render() {
         return (
             <Aux>
                 <Toolbar 
-                    drawerToggleClicked={this.sideDrawerToggleHandler}
-                    isAuth={this.props.isAuthenticated} />
+                    drawerToggleClicked={sideDrawerToggleHandler}
+                    isAuth={props.isAuthenticated} />
                 <SideDrawer 
-                    isAuth={this.props.isAuthenticated}
-                    open={this.state.showSideDrawer} 
-                    closed={this.sideDrawerClosedHandler} />
+                    isAuth={props.isAuthenticated}
+                    open={sideDrawerIsVisible} 
+                    closed={sideDrawerClosedHandler} />
                 <main className={classes.Content}>
-                    {this.props.children}
+                    {props.children}
                 </main>
             </Aux>
         )
-    }
 }
 
 const mapStateToProps = state => {
--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -1,48 +1,44 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Modal from '../../components/UI/Modal/Modal';
 import Aux from '../Auxilliary/Auxilliary';
 
 const withErrorHandler = (WrappedCompnent, axios) => {
-    return class extends Component {
+    return props => {
+        const [error, setError] = useState(null);
+    
+        const reqInterceptor = axios.interceptors.request.use(req => {
+            setError(null);
+            return req;
+        });
+    
+        const resInterceptor = axios.interceptors.response.use(res => res, errors => {
+            setError(errors);
+        });
+    
+        useEffect(() => {
+            return () => {
+                axios.interceptors.request.eject(reqInterceptor);
+                axios.interceptors.response.eject(resInterceptor);
+            };
+        }, [reqInterceptor, resInterceptor]);
+    
+        const errorConfirmedHandler = () => {
+            setError(null);
+        };
+        
 
-        state = {
-            error: null
-        }
-
-        componentWillMount () {
-            this.reqInterceptor = axios.interceptors.request.use(req => {
-                this.setState({error: null});
-                return req;
-            });
-            this.resInterceptor = axios.interceptors.response.use(res => res, error => {
-                this.setState({error: error});
-            });
-        }
-
-        componentWillUnmount () {
-            console.log('Will Unmount', this.reqInterceptor, this.resInterceptor);
-            axios.interceptors.request.eject(this.reqInterceptor);
-            axios.interceptors.response.eject(this.resInterceptor);
-        }
-
-        errorConfirmedHandler = () => {
-            this.setState({error: null});
-        }
-
-        render () {
-            return (
-                <Aux>
-                    <Modal 
-                        show={this.state.error}
-                        modalClosed={this.errorConfirmedHandler} >
-                        {this.state.error ? this.state.error.message : null}
-                    </Modal>
-                    <WrappedCompnent {...this.props} />
-                </Aux>
-            )
-        }
-    }
-} 
+        return (
+            <Aux>
+                <Modal 
+                    show={error}
+                    modalClosed={errorConfirmedHandler} >
+                    {error ? error.message : null}
+                </Modal>
+                <WrappedCompnent {...props} />
+            </Aux>
+        );
+    };
+};
 
 export default withErrorHandler;
